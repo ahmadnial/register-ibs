bdb4a520d47831c232c0d04f73c2ee9bf3ebe4ce
ahmadnial <ahmadhky@gmail.com>
2025-01-03 14:07:13 +0700

    fix patch part5

--- index.php
+++ index.php
@@ -144,7 +144,7 @@ include "proses.php"; ?>
                                         <option value="">--Select Me--</option>
                                         <option value="dr.BINTANG BERLIAN,SP.AN">dr.BINTANG BERLIAN,SP.AN</option>
                                         <option value="dr.BOWO ARDIYANTO,SpAn">dr.BOWO ARDIYANTO,SpAn</option>
-                                        <option value="dr.Syaeful Fatah,Sp.An">dr.Syaeful Fatah,Sp.An</option>
+                                        <!-- <option value="dr.Syaeful Fatah,Sp.An">dr.Syaeful Fatah,Sp.An</option> -->
                                         <option value="dr.Adi Hidayat,Sp.An">dr.Adi Hidayat,Sp.An</option>
                                         <option value="dr.Dika Rezkiawan Sp.An">dr.Dika Rezkiawan,Sp.An</option>
                                         <option value="-">Tidak ada</option>
